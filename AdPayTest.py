5589201fd8319bec3d9232632c772e9e8b71a007
Zach Schmitt <Zach.Schmitt@vu.com>
2022-01-25 18:51:10 -0600

    Assignment 3.2- Take 2

--- AdPayTest.py
+++ AdPayTest.py
@@ -1,13 +1,12 @@
 try:
     hrs = float(input("Enter Hours:"))
     rate = float(input("Enter Pay Rate:"))
+    if hrs > 40 :
+        regpay = hrs*rate
+        otpay = (hrs - 40.0) * (.5 * rate)
+        pay = regpay + otpay
+    else:
+        pay = hrs*rate
+    print("Pay:",pay)
 except:
     print("Error, please enter numeric input"),quit()
-
-if hrs > 40 :
-    regpay = hrs*rate
-    otpay = (hrs - 40.0) * (.5 * rate)
-    pay = regpay + otpay
-else:
-    pay = hrs*rate
-print("Pay:",pay)
