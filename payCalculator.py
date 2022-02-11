bc0f6fa21b6024c2a1668c01a14f56bea8030494
Zach Schmitt <zach.schmitt@vu.com>
2022-02-10 22:08:24 -0600

    3.2 take 3

--- payCalculator.py
+++ payCalculator.py
@@ -1,24 +1,24 @@
 def calculatePay():
     
     # This first line is provided for you
-hrs = input("Enter Hours: ")
+    hrs = input("Enter Hours: ")
 
-try:
-    float(hrs)
     try:
-        rate = input("Enter Pay Rate:")
-        float(rate)
-        if float(hrs) > 40 :
-            regpay = 40 * float(rate)
-            otpay = (float(hrs) - 40.0) * (float(rate)*1.5)
-            pay = regpay + otpay
-        else:
-            pay = float(hrs) * float(rate)
-        print("Pay:", pay)
+        float(hrs)
+        try:
+            rate = input("Enter Pay Rate:")
+            float(rate)
+            if float(hrs) > 40 :
+                regpay = 40 * float(rate)
+                otpay = (float(hrs) - 40.0) * (float(rate)*1.5)
+                pay = regpay + otpay
+            else:
+                pay = float(hrs) * float(rate)
+            print("Pay:", pay)
+        except:
+            print("Error, please enter numeric input")
     except:
         print("Error, please enter numeric input")
-except:
-    print("Error, please enter numeric input")
     # end assignment
 
 ## if you want to test locally before you try to sync
